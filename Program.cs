a1573e64cffe34314e93adee19959094f06886e9
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-04-27 11:30:15 +0200

    completed with improvements

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// System.Console.WriteLine("Hello, World!");
